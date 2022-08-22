148f84135c7bc6190a49040ecf6a9ec7bf32f5c7
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-22 17:06:28 +0800

    home section done

--- app.js
+++ app.js
@@ -1,33 +1,29 @@
 window.onscroll = () => {
-    searchForm.classList.remove('active');
+  searchForm.classList.remove("active");
 
-    if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
-    }
-    else {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
-}
-
-window.onload = () => {
-    if (window.scrollY > 80) {
-        document.querySelector('.header .header-2').classList.add('active');
-    }
-    else {
-        document.querySelector('.header .header-2').classList.remove('active');
-    }
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
 };
 
+window.onload = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.remove("active");
+  }
+};
 
 // search box
 
-const searchForm = document.querySelector('.search-form');
+const searchForm = document.querySelector(".search-form");
 const searchBtn = document.querySelector("#search-btn");
 
 searchBtn.onclick = () => {
-    searchForm.classList.toggle('active');
-}
-
+  searchForm.classList.toggle("active");
+};
 
 // login form
 
@@ -36,10 +32,31 @@ const loginBtn = document.querySelector("#login-btn");
 const closeLoginBtn = document.querySelector("#close-login-btn");
 
 loginBtn.onclick = () => {
-    loginForm.classList.toggle('active');
-}
+  loginForm.classList.toggle("active");
+};
 
 closeLoginBtn.onclick = () => {
-    loginForm.classList.remove('active');
-}
+  loginForm.classList.remove("active");
+};
+
+// swiper
+var swiper = new Swiper(".book-slider", {
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
 
