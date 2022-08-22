65a813c3ab5035cb960b2341169b143383d843b1
MD ASHIK MAHMUD <ashik.swfu@outlook.com>
2022-08-22 18:44:37 +0800

    deal

--- app.js
+++ app.js
@@ -89,3 +89,26 @@ var swiper = new Swiper(".featured-slider", {
   },
 });
 
+
+// swiper arrivals-slider
+var swiper = new Swiper(".arrivals-slider", {
+  spaceBetween: 10,
+  loop: true,
+  centeredSlides: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  breakpoints: {
+    0: {
+      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
+    },
+    1024: {
+      slidesPerView: 3,
+    },
+  },
+});
+
